7511126ef28cd8727f826119feac832ba2d08022
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-10-25 13:33:17 +0200

    first commit

---
+++ src/test/java/com/carrentalfrontend/CarRentalFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentalfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
